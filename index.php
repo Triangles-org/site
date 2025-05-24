c3c6b73fbf248fb18433caaa7764c243495b404b
Thepuppetqueen57 <doggyandkira2013@gmail.com>
2025-05-23 21:22:42 -0700

    Removed an angry little gay comment
    that comment was just lncvrt telling me how to not be bad

--- index.php
+++ index.php
@@ -13,4 +13,3 @@ ob_start();
 $content = ob_get_clean();
 $title = 'Home';
 include __DIR__ . '/layout.php';
-// if copy this to like info folder for example do like '/../layout.php' not '/layout.php'
