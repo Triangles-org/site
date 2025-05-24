555f2ac53a16928c0dcecca9f289beb97bdb34e7
Thepuppetqueen57 <doggyandkira2013@gmail.com>
2025-05-23 21:28:08 -0700

    Size and positioning improvements
    Some suggestions came from lncvrt

--- index.php
+++ index.php
@@ -2,10 +2,10 @@
 ob_start();
 ?>
 <div id="main-div">
-    <h1 style="margin-top: 300px;">Triangles</h1>
+    <h1 style="margin-top: 200px;">Triangles</h1>
     <p>Developer of the game: Lncvrt</p>
     <p>Developer of the website: Puppet</p>
-    <h1 style="margin-top: 100px;">Links</h1>
+    <h2 style="margin-top: 100px;">Links</h2>
     <p><a href="/discord">Discord</a></p>
     <p><a href="/download/">Download</a></p>
 </div>
