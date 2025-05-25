d61708941c59b3ed11022945bf0b4dc8a073aa27
Lncvrt <lncvrtreal@gmail.com>
2025-05-25 12:11:22 -0700

    Improving font size and spacing

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ ob_start();
     <h1 style="margin-top: 200px;">Triangles</h1>
     <p>Developer of the game: Lncvrt</p>
     <p>Developers of the website: Puppet & Lncvrt</p>
-    <h2 style="margin-top: 100px;">Links</h2>
+    <h2 style="margin-top: 50px;">Links</h2>
     <p class="link link-left"><a href="/discord">Discord</a></p>
     <p class="link link-right"><a href="/download/">Download</a></p>
 </div>
