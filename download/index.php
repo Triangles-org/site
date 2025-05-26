603d537f355c134a7400d33917b1fbc2cd40c412
Lncvrt <lncvrtreal@gmail.com>
2025-05-26 16:38:38 -0700

    Remove macos as it is broken right now

--- download/index.php
+++ download/index.php
@@ -8,7 +8,6 @@ ob_start();
     <p class="link link-right"><a href="https://triangles-cdn.lncvrt.xyz/Triangles-1.0.0-beta4-win-x86.zip"><b>Windows x86 download</b></a></p>
     <p class="link link-left"><a href="https://triangles-cdn.lncvrt.xyz/Triangles-1.0.0-beta4-win-arm64.zip"><b>Windows ARM64 download</b></a></p>
     <p class="link link-right"><a href="https://triangles-cdn.lncvrt.xyz/Triangles-1.0.0-beta4-linux.zip"><b>Linux (x86 or x64) download</b></a></p>
-    <p class="link link-left"><a href="https://triangles-cdn.lncvrt.xyz/Triangles-1.0.0-beta4-macos.zip"><b>MacOS (Intel or Silicon) download</b></a></p>
     <p>If you don't know what download to use, select <b>Windows x64</b> or <b>Windows x86</b> if that doesn't work.</p>
 </div>
 <?php
